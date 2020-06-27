a192c35269e83e8268597f7d90dbd3575144bd37
Fernando Kenji <kenjifernandoo@gmail.com>
2020-06-26 22:11:16 -0300

    feat: Add Validation

--- src/modules/users/infra/http/routes/profile.routes.ts
+++ src/modules/users/infra/http/routes/profile.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import ProfileController from '../controllers/ProfileController';
 
@@ -10,6 +11,27 @@ const profileController = new ProfileController();
 profileRouter.use(ensureAuthenticated);
 
 profileRouter.get('/', profileController.show);
-profileRouter.put('/', profileController.update);
+
+profileRouter.put(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required().min(3),
+      email: Joi.string().email().required(),
+      old_password: Joi.string(),
+      password: Joi.when('old_password', {
+        not: '',
+        then: Joi.string().required().min(6),
+        otherwise: Joi.any(),
+      }),
+      password_confirmation: Joi.when('old_password', {
+        not: '',
+        then: Joi.string().required().valid(Joi.ref('password')),
+        otherwise: Joi.any(),
+      }),
+    },
+  }),
+  profileController.update,
+);
 
 export default profileRouter;
